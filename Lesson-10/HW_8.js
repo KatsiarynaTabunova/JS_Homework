9dbf3471ef584b9ac33d5b19be11c3a16bbb7023
DESKTOP-13F6AGA\Катя <katepilipionak18.90@gmail.com>
2022-05-20 18:55:00 +0300

    HW to check

---
+++ Lesson-10/HW_8.js
@@ -0,0 +1,82 @@
+//1
+function filterNumbersArr(arr) {
+  var newArr = [];
+
+  arr.forEach(function (item) {
+    if (item > 0) {
+      newArr.push(item);
+    }
+  });
+
+  return newArr;
+}
+console.log(filterNumbersArr([-1, 0, 2, 34, -2]));
+
+//2
+function findFirstPositiveNumber(arr) {
+  return arr.find(function (item) {
+    return item > 0;
+  });
+}
+console.log(findFirstPositiveNumber([-1, 0, 2, 34, -2]));
+
+//3
+function isPalindrome(word) {
+  var changingWord = word.split("").reverse().join("").toLowerCase();
+
+  return word.toLowerCase() == changingWord;
+}
+console.log(isPalindrome("шалаШ"));
+console.log(isPalindrome("привет"));
+
+//4
+function areAnagrams(word1, word2) {
+  var a = word1.toLowerCase().split("").sort().join();
+  var b = word2.toLowerCase().split("").sort().join();
+
+  return a == b;
+}
+console.log(areAnagrams("кот", "Отк"));
+console.log(areAnagrams("кот", "атк"));
+console.log(areAnagrams("кот", "отко"));
+
+//5
+//1 способ
+function divideArr(arr, lengthNewArrs) {
+  newArr = [];
+  for (var i = 0; i < arr.length; i += lengthNewArrs) {
+    newArr.push(arr.slice(i, i + lengthNewArrs));
+  }
+  return newArr;
+}
+//2 способ
+function divideArr(arr, lengthNewArrs) {
+  var newArr = [];
+  while (arr.length > 0) {
+    newArr.push(arr.splice(0, lengthNewArrs));
+  }
+  return newArr;
+}
+console.log(divideArr([1, 2, 3, 4], 2));
+console.log(divideArr([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
+//6
+//1 способ(сначала поломала голову над первым решением, а потом нашла второй:)
+function isPossibleDegreeOfTwo(number) {
+  while (number >= 2) {
+    number = number / 2;
+    console.log(number);
+  }
+  if (number === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isPossibleDegreeOfTwo(5));
+
+//2 способ
+function isPossibleDegreeOfTwo(number) {
+  return number % 2 == 0;
+}
+console.log(isPossibleDegreeOfTwo(8));
