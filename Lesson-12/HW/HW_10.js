f79119f8cf77e97a244664c02c9088677affd922
DESKTOP-13F6AGA\Катя <katepilipionak18.90@gmail.com>
2022-05-31 12:22:08 +0300

    changing after checking

--- Lesson-12/HW/HW_10.js
+++ Lesson-12/HW/HW_10.js
@@ -1,5 +1,5 @@
-var table = document.createElement("table"),
-  tbody = document.createElement("tbody");
+var table = document.createElement('table'),
+    tbody = document.createElement('tbody');
 
 document.body.appendChild(table);
 table.appendChild(tbody);
@@ -7,11 +7,11 @@ table.appendChild(tbody);
 var currentTablerow;
 
 var button = createTableRows(1, 1);
-button.cells[0].innerText = "Добавить";
+button.cells[0].innerText = '"Добавить"';
 button.cells[0].colSpan = 3;
 
-button.addEventListener("click", function () {
-  createTableRows(3, 1);
+button.addEventListener('click', function () {
+    createTableRows(3, 1);
 });
 
 createTableRows(3, 1);
@@ -19,55 +19,54 @@ createTableRows(3, 1);
 createTableRows(3, 1);
 
 function createTableRows(cols, rows) {
-  for (var i = 0; i < rows; i++) {
-    var tr = document.createElement("tr");
-    for (var k = 0; k < cols; k++) {
-      var td = document.createElement("td");
+    for (var i = 0; i < rows; i++) {
+        var tr = document.createElement('tr');
+        for (var k = 0; k < cols; k++) {
+            var td = document.createElement('td');
 
-      tr.appendChild(td);
-      td.style.cssText =
-        "border: 1px solid black; height: 40px; width: 250px; padding: 10px; text-align: center; font-size: 20px";
+            tr.appendChild(td);
+            td.className = 'td';
+        }
+        if (currentTablerow == undefined) {
+            tbody.appendChild(tr);
+        } else {
+            tbody.insertBefore(tr, currentTablerow);
+        }
     }
-    if (currentTablerow == undefined) {
-      tbody.appendChild(tr);
-    } else {
-      tbody.insertBefore(tr, currentTablerow);
-    }
-  }
-  currentTablerow = tr;
-  return tr;
+    currentTablerow = tr;
+    return tr;
 }
 
-table.style.borderCollapse = "collapse";
+table.style.borderCollapse = 'collapse';
 
-var input = document.createElement("input");
-input.type = "text";
-input.style.cssText = "border: none; height: 30px; width: 200px";
+var input = document.createElement('input');
+input.type = 'text';
+input.className = 'input';
 
-table.addEventListener("click", function (event) {
-  if (event.target != button.cells[0]) {
-    var text = event.target.textContent;
+table.addEventListener('click', function (event) {
+    if (event.target != button.cells[0]) {
+        var text = event.target.textContent;
 
-    event.target.textContent = null;
-    event.target.appendChild(input).focus();
-    input.value = text;
-  }
+        event.target.textContent = null;
+        event.target.appendChild(input).focus();
+        input.value = text;
+    }
 });
 
-table.addEventListener("focusout", function (event) {
-  event.target.parentElement.innerHTML = event.target.value;
-  event.target.value = null;
+table.addEventListener('focusout', function (event) {
+    event.target.parentElement.innerHTML = event.target.value;
+    event.target.value = null;
 });
 
 //*
-table.addEventListener("keyup", function (event) {
-  if (event.keyCode == 13) {
-    if (event.target != button.cells[0]) {
-      var text = event.target.value;
-      event.target.textContent = null;
+table.addEventListener('keyup', function (event) {
+    if (event.keyCode == 13) {
+        if (event.target != button.cells[0]) {
+            var text = event.target.value;
+            event.target.textContent = null;
 
-      var perernt = event.target.parentElement;
-      perernt.textContent = text;
+            var perernt = event.target.parentElement;
+            perernt.textContent = text;
+        }
     }
-  }
 });
--- Lesson-12/Lesson-prectice/script.js
+++ Lesson-12/Lesson-prectice/script.js
@@ -1,26 +1,28 @@
-var container = document.getElementById("container");
-var buttons = document.getElementsByTagName("button");
+var container = document.getElementById('container');
+var buttons = document.getElementsByTagName('button');
 
-var firstPar = document.createElement("p"),
-  secondPar = document.createElement("p");
+var firstPar = document.createElement('p'),
+    secondPar = document.createElement('p');
 
 firstPar.innerHTML =
-  'Hello, here are <a href="https://www.facebook.com" >Link 1</a> and <a href="https://twitter.com">Link 2</a>';
+    'Hello, here are <a href="https://www.facebook.com" >Link 1</a> and <a href="https://twitter.com">Link 2</a>';
 secondPar.innerHTML =
-  'Hello, here are <a href="http://google.by">Link 3</a> and <a href="https://vk.com">Link 4</a>';
+    'Hello, here are <a href="http://google.by">Link 3</a> and <a href="https://vk.com">Link 4</a>';
 
 container.appendChild(firstPar);
 container.appendChild(secondPar);
 
-buttons[0].addEventListener("click", changeColor);
+buttons[0].addEventListener('click', changeColor);
 
 function changeColor() {
-  for (var item of firstPar.children) {
-    item.classList.toggle("changed");
-  }
+    for (var i = 0; i < firstPar.children.length; i++) {
+        firstPar.children[i].classList.toggle('changed');
+    }
 }
 
 secondPar.onclick = function func(event) {
-  event.preventDefault();
-  alert(event.srcElement.href);
+    if (event.target.href != undefined) {
+        event.preventDefault();
+        alert(event.target.href);
+    }
 };
