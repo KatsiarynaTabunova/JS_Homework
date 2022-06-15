027edd24f3bcb126780a994d3e6135d9b333b7c1
tabunova.katsiaryna <katepilipionak18.90@gmail.com>
2022-06-15 21:55:41 +0300

    chenging after checking

--- Lesson-16/Homework-14/HW.js
+++ Lesson-16/Homework-14/HW.js
@@ -1,14 +1,15 @@
-//1
-/^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@([\da-z]{1,10}[\-\.]?[\da-z]{1,10}).com$/gi.test(
+// //1
+/^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@([\da-z]{1,10}[\-\.]?[\da-z]{1,10}).com$/i.test(
     'name_surname-1234@gmail.com'
 );
 
-//2
+// //2
 function checkPhoneNumber(number) {
-    var result = +number.search(
-        /(((\+?375-|8-0)(25|29|33|44|17))-[1-9]{1}[\d]{2}-[\d]{2}-[\d]{2})|(((\+?375|80)(25|29|33|55|17))[1-9]{1}[\d]{6})/
-    );
-    return result === 0;
+    var result =
+        /^((\+?375-|8-?0)(25|29|33|44|17))-?[1-9][\d]{2}(-?[\d]{2}){2}$/.test(
+            number
+        );
+    return result;
 }
 console.log(checkPhoneNumber('+375-25-777-77-77'));
 
@@ -17,30 +18,39 @@ console.log(checkPhoneNumber('+375-25-777-77-77'));
 function getNumberOfVowels(text) {
     var result = text.toLowerCase().match(/[аеёиоуыэюя]/g);
 
+    if (result === null) {
+        return 0;
+    }
     return result.length;
 }
 console.log(getNumberOfVowels('Во двОре растЕт трава'));
 
-//2 способ
+// //2 способ
 function getNumberOfVowels(text) {
     var result = text.match(/[аеёиоуыэюяАЕЁИОУЫЭЮЯ]/g);
 
+    if (result === null) {
+        return 0;
+    }
     return result.length;
 }
 console.log(getNumberOfVowels('Во двОре растЕт трава'));
 
-//3 способ
+// //3 способ
 function getNumberOfVowels(text) {
     var changedText = text.replace(/А|Е|Ё|И|О|У|Ы|Э|Ю|Я/gi, function (text) {
         return text.toLowerCase();
     });
     var result = changedText.match(/[аеёиоуыэюя]/g);
 
+    if (result === null) {
+        return 0;
+    }
     return result.length;
 }
 console.log(getNumberOfVowels('Во двОре растЕт трава'));
 
-//4 способ
+// //4 способ
 function getNumberOfVowels(text) {
     var result = text
         .toLowerCase()
